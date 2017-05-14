0aeed0e64818261fb348ba0eb302c01fb6db2c70
wmgcy199464 <572298529@qq.com>
2017-05-14 15:05:02 +0800

    更新js文件

--- testXml.js
+++ testXml.js
@@ -1,43 +1,56 @@
-function validateXML(txt)
+function validateXML(xmlcode)
 {
-// code for IE
-  alter("Hello");
-if (window.ActiveXObject)
-  {
-  var xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-  xmlDoc.async="false";
-  xmlDoc.loadXML(document.all(txt).value);
+    //errorCode 0是xml正确，1是xml错误，2是无法验证
+    var xmlDoc,errorMessage,errorCode = 0;
+    // code for IE
+	  var xmlContent=xmlcode.value;
+    if(window.ActiveXObject)
+    {
+        xmlDoc  = new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async="false";
+        xmlDoc.loadXML(xmlContent);
 
-  if(xmlDoc.parseError.errorCode!=0)
-    {
-    txt="Error Code: " + xmlDoc.parseError.errorCode + "\n";
-    txt=txt+"Error Reason: " + xmlDoc.parseError.reason;
-    txt=txt+"Error Line: " + xmlDoc.parseError.line;
-    alert(txt);
+        if(xmlDoc.parseError.errorCode!=0)
+        {
+            errorMessage="错误code: " + xmlDoc.parseError.errorCode + "\n";
+            errorMessage=errorMessage+"错误原因: " + xmlDoc.parseError.reason;
+            errorMessage=errorMessage+"错误位置: " + xmlDoc.parseError.line;
+            errorCode = 1;
+        }
+        else
+        {
+            errorMessage = "格式正确";
+        }
     }
-  else
+    // code for Mozilla, Firefox, Opera, chrome, safari,etc.
+    else if (document.implementation.createDocument)
     {
-    alert("No errors found");
+        var parser=new DOMParser();
+        xmlDoc = parser.parseFromString(xmlContent,"text/xml");
+        var error = xmlDoc.getElementsByTagName("parsererror");
+        if (error.length > 0)
+        {
+               if(xmlDoc.documentElement.nodeName=="parsererror"){
+                errorCode = 1;
+                errorMessage = xmlDoc.documentElement.childNodes[0].nodeValue;
+            } else {
+                errorCode = 1;
+                errorMessage = xmlDoc.getElementsByTagName("parsererror")[0].innerHTML;
+            }
+        }
+        else
+        {
+            errorMessage = "格式正确";
+        }
     }
-  }
-// code for Mozilla, Firefox, Opera, etc.
-else if (document.implementation.createDocument)
-  {
-var parser=new DOMParser();
-var text=document.getElementById(txt).value;
-var xmlDoc=parser.parseFromString(text,"text/xml");
-
-  if (xmlDoc.documentElement.nodeName=="parsererror")
+    else
     {
-    alert(xmlDoc.documentElement.childNodes[0].nodeValue);
+        errorCode = 2;
+        errorMessage = "浏览器不支持验证，无法验证xml正确性";
     }
-  else
-    {
-    alert("No errors found");
-    }
-  }
-else
-  {
-  alert('Your browser cannot handle XML validation');
-  }
+	alert(errorMessage);
+    return {
+        "msg":errorMessage, 
+        "error_code":errorCode
+    };
 }
